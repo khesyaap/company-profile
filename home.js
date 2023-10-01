035000e0dd2ae2414ef95591c270432fa0c1cd8d
khesya <90244477+khesyaap@users.noreply.github.com>
2023-10-01 11:30:54 +0700

    Add files via upload

--- home.js
+++ home.js
@@ -1,20 +1,22 @@
-function sendMessage(){
-    (function(){
-        emailjs.init("tYTwkC6LKP3HqG-PB"); // Account Public Key
-    })();
+// Ini untuk menyambungkan ke gmail lewat emailjs (cuman belum bisa terjalani)
 
-    var serviceID = "service_6uztmk6"; // Email Service ID
-    var templateID = "template_ga7bxa2"; // Email Template ID
+// function sendMessage(){
+//     (function(){
+//         emailjs.init("tYTwkC6LKP3HqG-PB"); // Account Public Key
+//     })();
 
-    var params = {
-        sendername: document.querySelector("#name").value,
-        senderemail: document.querySelector("#email").value,
-        message: document.querySelector("#message").value
-    };
+//     var serviceID = "service_6uztmk6"; // Email Service ID
+//     var templateID = "template_ga7bxa2"; // Email Template ID
 
-    emailjs.send(serviceID, templateID, params)
-    .then( Response => {
-        alert('Thank You,' + params['sendername'] + '! Your message has been sent.');
-    })
-    .catch();
-}
+//     var params = {
+//         sendername: document.querySelector("#name").value,
+//         senderemail: document.querySelector("#email").value,
+//         message: document.querySelector("#message").value
+//     };
+
+//     emailjs.send(serviceID, templateID, params)
+//     .then( Response => {
+//         alert('Thank You,' + params['sendername'] + '! Your message has been sent.');
+//     })
+//     .catch();
+// }
---
+++ our.js
@@ -0,0 +1,65 @@
+// Our Product
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  // Our Client
+$(document).ready(function(){
+  $('.customer-logos2').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover:false,
+      responsive: [{
+          breakpoint: 768,
+          setting: {
+              slidesToShow:4
+          }
+      }, {
+          breakpoint: 520,
+          setting: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
+
+
+// Our Partner
+$(document).ready(function(){
+    $('.customer-logos1').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover:false,
+        responsive: [{
+            breakpoint: 768,
+            setting: {
+                slidesToShow:4
+            }
+        }, {
+            breakpoint: 520,
+            setting: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
